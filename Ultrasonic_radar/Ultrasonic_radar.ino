65371346dd7f5e59f359f8d0d611f5d5aed19091
Ding_YF <DingYFA@yeah.net>
2021-10-04 11:02:52 +0800

    接通超声波模块_返回30cm以内的距离信息

--- Ultrasonic_radar/Ultrasonic_radar.ino
+++ Ultrasonic_radar/Ultrasonic_radar.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+const int trigPin = 10;
+const int echoPin = 11;
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i=15;i<=165;i+=15){
+  distance = 1000;
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(',');
+  Serial.print(distance);
+  Serial.println('.');
+  }
+}
+
+int calculateDistance(){
+
+    digitalWrite(trigPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin,LOW);
+    duration = pulseIn(echoPin,HIGH);
+    distance = duration*0.034/2;
+    if (distance <= 30)
+        return distance;
+    else 
+      return -1;
 
 }
